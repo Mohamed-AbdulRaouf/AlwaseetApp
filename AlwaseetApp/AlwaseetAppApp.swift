b7585da32671e159c3669ffba94199a4aa314ed6
Mohamed Abdul-Raouf <mohamed.a.raouf@icloud.com>
2022-06-29 14:59:47 +0200

    Initial Commit

---
+++ AlwaseetApp/AlwaseetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaseetAppApp.swift
+//  AlwaseetApp
+//
+//  Created by mohamed on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct AlwaseetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
